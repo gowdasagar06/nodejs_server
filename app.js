33351f51b330f0f27d8a91165899f830eddfc80a
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2023-06-01 12:25:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Sagar@123',
     database: 'Doctor_Appointment'
 });
 
